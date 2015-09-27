b1e73808940b04a966d1f64955aa66aa04237673
Rahul Krishna <i.m.ralk@gmail.com>
2015-09-27 15:59:29 -0400

    C45

---
+++ tools/PC45.py
@@ -0,0 +1,121 @@
+"""
+PC45 - Python C4.5 Framework
+````````````````````````````
+A Method for generating a pruned C4.5 decision tree. Based on,
+[1] Quinlan, J. Ross. "Improved use of continuous attributes in C4. 5." Journal of artificial intelligence research (1996): 77-90.
+[2] Quinlan, J. Ross. C4. 5: programs for machine learning. Elsevier, 2014.
+
+Also uses,
+[3] Usama M. Fayyad, Keki B. Irani. "Multi-interval discretization of continuous valued attributes for classification learning." Thirteenth International Joint Conference on Artificial Intelligence, 1022-1027, 1993.
+
+"""
+from __future__ import division, print_function
+from discretize import discretize, fWeight
+import pandas as pd
+import numpy as np
+from containers import Thing
+from collections import Counter
+from misc import explore, csv2DF
+from pdb import set_trace
+
+def dtree(tbl, rows=None, lvl=-1, asIs=10 ** 32, up=None, klass = -1, branch=[],
+          f=None, val=None, opt=None):
+
+  here = Thing(t=tbl, kids=[], f=f, val=val, up=up, lvl=lvl
+              , rows=rows, modes={}, branch=branch)
+
+  features = fWeight(tbl)
+
+  if opt.prune and lvl<0:
+    features = fWeight(tbl)[:int(len(features)*opt.infoPrune)]
+
+  name = features.pop(0)
+  remaining = tbl[features+[tbl.columns[opt.klass]]]
+  feature = tbl[name].values
+  klass = tbl[tbl.columns[opt.klass]].values
+  N = len(klass)
+  def mode(lst):
+    return np.max([Counter(lst)[k] for k in Counter(lst).keys()])
+  here.mode = mode(klass)
+  here.score = np.mean(klass)
+  splits = discretize(feature, klass)
+  LO, HI = min(feature), max(feature)
+  def pairs(lst):
+    while len(lst)>1:
+      yield (lst.pop(0), lst[0])
+  cutoffs = [t for t in pairs(sorted(list(set(splits+[LO,HI]))))]
+
+  if lvl>(opt.maxLvL if opt.prune else int(len(features)*opt.infoPrune)):
+    return here
+  if asIs == 0:
+    return here
+  if len(features)<1:
+    return here
+
+  def rows():
+    for span in cutoffs:
+      new=[]
+      for f, row in zip(feature, remaining.values.tolist()):
+        if span[0]<=f<span[1]:
+          new.append(row)
+        elif f==span[1]==HI:
+          new.append(row)
+      yield pd.DataFrame(new,columns=remaining.columns), span
+
+  def ent(x):
+    C = Counter(x)
+    N = len(x)
+    return sum([-C[n]/N*np.log(C[n]/N) for n in C.keys()])
+
+  for child, span in rows():
+    n = child.shape[0]
+    toBe = ent(child[child.columns[opt.klass]])
+    if opt.min<=n<N:
+      here.kids += [dtree(child, lvl=lvl + 1, asIs=toBe, up=here
+                          , branch= branch + [(name, span)]
+                          , f=name, val=span, opt=opt)]
+
+  return here
+  # # ------ Debug ------
+  # set_trace()
+
+def show(n, lvl=-1):
+  import sys
+  def say(x):
+    sys.stdout.write(x)
+  if n.f:
+    say(('|..' * lvl) + str(n.f) + "=" + str(n.val) +
+        "\t:" + str(n.score))
+  if n.kids:
+    print('')
+    for k in n.kids:
+      show(k, lvl + 1)
+  else:
+    print("")
+
+def _test():
+
+  tbl_loc = explore(name='ant')[0]
+  tbl = csv2DF(tbl_loc)
+
+  # Define Tree settings
+  opt = Thing(
+         min=1,
+         maxLvL=10,
+         infoPrune=0.33,
+         klass=-1,
+         prune=True,
+         debug=False,
+         verbose=True)
+
+  #Build a tree
+  tree = dtree(tbl, opt=opt)
+
+  # Show the tree
+  if opt.verbose: show(tree)
+
+  # ----- Debug? -----
+  if opt.debug: set_trace()
+
+if __name__=='__main__':
+  _test()
