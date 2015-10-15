a28bc519ab70e07a8b7a73cca12691cf8d353275
rahlk <i.m.ralk@gmail.com>
2015-10-15 09:09:41 -0400

    commit with a makefile

--- defects.py
+++ defects.py
@@ -5,63 +5,75 @@ from Planners.xtree import xtree
 from tools.sk import rdivDemo
 from tools.misc import explore, say
 
-def temporal():
-  for name in ['ant', 'ivy', 'jedit', 'lucene', 'poi']:
-    print("##", name)
-    e=[]
-    train, test = explore(dir='Data/Jureczko/', name=name)
-    for planners in [xtree, method1, method2, method3]:
-      aft = [planners.__doc__]
-      for _ in xrange(10):
-        aft.append(planners(train, test, justDeltas=False))
-        set_trace()
-      e.append(aft)
-    rdivDemo(e)
+class temporal:
+  def __init__(self):
+    pass
+  def deltas(self):
+    from collections import Counter
+    counts = {}
 
-def cross():
-  for name in ['ant', 'ivy', 'jedit', 'lucene', 'poi']:
-    print("##", name)
-    e=[]
-    train, test = explore(dir='Data/Jureczko/', name=name)
-    for planners in [xtree, method1, method2, method3]:
-      aft = [planners.__doc__]
-      for _ in xrange(10):
-        aft.append(planners(train, test, justDeltas=False))
-        set_trace()
-      e.append(aft)
-    rdivDemo(e)
-
-def deltas():
-
-  from collections import Counter
-  counts = {}
-
-  def save2plot(header, counts, labels, N):
-    for h in header: say(h+' ')
-    print('')
-    for l in labels:
-      say(l[1:]+' ')
-      for k in counts.keys():
-        say("%0.2f "%(counts[k][l]*100/N))
+    def save2plot(header, counts, labels, N):
+      for h in header: say(h+' ')
       print('')
+      for l in labels:
+        say(l[1:]+' ')
+        for k in counts.keys():
+          say("%0.2f "%(counts[k][l]*100/N))
+        print('')
 
 
-  for name in ['ant', 'ivy', 'jedit', 'lucene', 'poi']:
-    print("##", name)
-    e=[]
-    train, test = explore(dir='Data/Jureczko/', name=name)
-    for planners in [xtree, method1, method2, method3]:
-      # aft = [planners.__doc__]
-      for _ in xrange(1):
-        keys=[]
-        everything, changes = planners(train, test, justDeltas=True)
-        for ch in changes: keys.extend(ch.keys())
-        counts.update({planners.__doc__:Counter(keys)})
-    header = ['Features']+counts.keys()
-    save2plot(header, counts, everything, N=len(changes))
-    # set_trace()
-    print()
+    for name in ['ant', 'ivy', 'jedit', 'lucene', 'poi']:
+      print("##", name)
+      e=[]
+      train, test = explore(dir='Data/Jureczko/', name=name)
+      for planners in [xtree, method1, method2, method3]:
+        # aft = [planners.__doc__]
+        for _ in xrange(1):
+          keys=[]
+          everything, changes = planners(train, test, justDeltas=True)
+          for ch in changes: keys.extend(ch.keys())
+          counts.update({planners.__doc__:Counter(keys)})
+      header = ['Features']+counts.keys()
+      save2plot(header, counts, everything, N=len(changes))
+
+  def improve(self):
+    for name in ['ant', 'ivy', 'jedit', 'lucene', 'poi']:
+      print("##", name)
+      e=[]
+      train, test = explore(dir='Data/Jureczko/', name=name)
+      for planners in [xtree, method1, method2, method3]:
+        aft = [planners.__doc__]
+        for _ in xrange(10):
+          aft.append(planners(train, test, justDeltas=False))
+          # set_trace()
+        e.append(aft)
+      rdivDemo(e)
+
+class cross:
+  def __init__(self):
+    pass
+  def improve1(self):
+    """
+    Learn from all other projects. Compare the results.
+    :return:
+    """
+    names=['ant', 'ivy', 'jedit', 'lucene', 'poi']
+    for planners in [xtree]:#, method1, method2, method3]:
+      for one in names:
+        e=[]
+        for two in names:
+          print("##", two, one)
+          aft = [two]
+          train,_ = explore(dir='Data/Jureczko/', name=two)
+          test,_  = explore(dir='Data/Jureczko/', name=one)
+          for _ in xrange(10):
+            aft.append(planners(train, test, justDeltas=False))
+            # set_trace()
+          e.append(aft)
+        rdivDemo(e)
+      set_trace()
 
 if __name__=='__main__':
-  temporal()
-  # deltas()
+
+  cross().improve1()
+  # temporal().deltas()
