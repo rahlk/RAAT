0b92316681abdbe6c84b79b37d176c7f48a03e55
amritbhanu <amritbhanu@gmail.com>
2015-10-22 10:07:19 -0400

    commit with a makefile

--- config.py
+++ config.py
@@ -6,17 +6,18 @@ from tools.sk import rdivDemo
 from tools.misc import explore
 
 def main():
+  e=[]
   for name in ['Apache', 'BDBC', 'BDBJ', 'LLVM', 'X264', 'SQL']:
     print("##", name)
-    e=[]
     data = explore(dir='Data/Seigmund/', name=name)
     # set_trace()
     for planners in [xtree]:#, method1, method2, method3]:
-      aft = [planners.__doc__]
-      for _ in xrange(1):
+      # aft = [planners.__doc__]
+      aft = [name]
+      for _ in xrange(10):
         aft.append(planners(train=data, test=None, config=True))
       e.append(aft)
-    rdivDemo(e)
+  rdivDemo(e)
     # set_trace()
 
 if __name__=='__main__':
